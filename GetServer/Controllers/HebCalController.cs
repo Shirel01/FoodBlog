bc67f75e6452cafe045005abc7e7fa0be7ef6c36
hadas <MWSYWBHDS@GMAIL.COM>
2022-09-28 23:55:51 +0300

    HebCal

---
+++ GetServer/Controllers/HebCalController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GetServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HebCalController : ControllerBase
+    {
+        // GET: api/<HebCalController>
+        [HttpGet]
+        public IEnumerable< string> Get()
+        {
+            /*נשלח פה את המיקום הנוכחי של האדם שעל זה נצטרך לקבל מידע
+               למרות שזה לא משנה אם זה רק בארץ*/
+            string city = "Haifa";
+
+            HebCal.BL.HebCalLogic bl = new HebCal.BL.HebCalLogic();
+
+            IEnumerable<string> answer = bl.IsHolidyWeek(city);//מחזיר את רשימת כל החגים הקרובים
+
+            return answer; 
+        }
+
+        // GET api/<HebCalController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HebCalController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
---
+++ HebCal.BL/HebCalLogical.cs
@@ -0,0 +1,42 @@
+﻿using HebCal.DP;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace HebCal.BL
+{
+    public class HebCalLogic
+    {
+
+        public IEnumerable<string> IsHolidyWeek(string City)
+        {
+           List<string> HolidayList =new List<string>();
+
+            DAL.HebCalAdapter dal = new DAL.HebCalAdapter();
+
+            Root myHolyday = null;
+
+            string myJson = dal.GetComingWeek(City);
+            if (myJson != null)
+            {
+                myHolyday = JsonConvert.DeserializeObject<Root>(myJson);
+            }
+
+            foreach (var item in myHolyday.items)
+            {
+                if (item.category == "holiday")
+                {
+                    string holiday = item.hebrew;
+
+                    HolidayList.Add(holiday);//רשימה של כל החגים הקרובים
+
+                }
+            }
+            return HolidayList;
+
+
+        }
+    }
+}
